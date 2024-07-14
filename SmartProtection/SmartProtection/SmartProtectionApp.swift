6d35251b18b5d7a4f3ce4a4d4d1bd4f81851d16d
Wojciech Kulas <w.kulas2313@gmail.com>
2024-07-14 17:27:38 +0200

    Project Skeleton.

---
+++ SmartProtection/SmartProtection/SmartProtectionApp.swift
@@ -0,0 +1,19 @@
+//
+//  SmartProtectionApp.swift
+//  SmartProtection
+//
+//  Created by Wojciech Kulas on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SmartProtectionApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
